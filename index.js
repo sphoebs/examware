e57eb3485253ff5fdddd947a16438b08710720a0
fabio casati <fab@fabios-MacBook-Pro.local>
2017-12-07 13:52:27 +0100

    clean early toy files

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const assignments = require('./assignments')
 
 
 
-app.use(function (req, res, next) {
-    console.log('\n Request IP Address: ', req.ip, 'Time:', Date.now())
-    next()
-})
+// app.use(function (req, res, next) {
+//     console.log('\n Request IP Address: ', req.ip, 'Time:', Date.now())
+//     next()
+// })
 
 app.use('/assignments', assignments)
 
@@ -28,7 +28,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 
-var students = []
+// var students = []
 
 
 app.get('/', function(request, response) {
@@ -37,38 +37,37 @@ app.get('/', function(request, response) {
 
 });
 
-app.get('/about', function(request, response) {
-    // response.render('pages/index');
-    response.send('about')
+// app.get('/about', function(request, response) {
+//     // response.render('pages/index');
+//     response.send('about')
+//
+// });
 
-});
+// app.get('/student/:id', function(req, res) {
+//     res.send('student ' + req.params.id);
+// });
+//
+// app.put('/student/:id', function(req, res) {
+//     res.send('student ' + req.params.id);
+// });
 
-app.get('/student/:id', function(req, res) {
-    res.send('student ' + req.params.id);
-});
-
-app.put('/student/:id', function(req, res) {
-    res.send('student ' + req.params.id);
-});
-
-app.post('/assignment/', function(req, res) {
 
     // var ip = (req.headers['x-forwarded-for'] ||
     // req.connection.remoteAddress ||
     // req.socket.remoteAddress ||
     // req.connection.socket.remoteAddress).split(",")[0];
 
-    var vip = req.ip
-
-    console.log(req.body)
-    res.send(req.body);
-});
-
-app.post('/test/:studentID', function(req, res) {
-
-    // fetch url and run test cases
-
-});
+//     var vip = req.ip
+//
+//     console.log(req.body)
+//     res.send(req.body);
+// });
+//
+// app.post('/test/:studentID', function(req, res) {
+//
+//     // fetch url and run test cases
+//
+// });
 
 
 app.listen(app.get('port'), function() {
